d40836c3393d2881e1a81460fe07d5d4c2fb9bfe
nearworld <artemismars2@outlook.com>
2023-04-03 09:25:59 +0900

    feat: generating shapes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
 export default function App() {
-  return <div>App</div>;
+  function generateShapes(
+    kind: "rectangle" | "circle",
+    backgroundColor: string
+  ) {
+    if (kind === "rectangle") {
+      console.log(kind);
+    }
+    if (kind === "circle") {
+      console.log(kind);
+    }
+
+    const shapes = [];
+    for (let i = 0; i < 100; i++) {
+      shapes.push(
+        <div
+          style={{
+            backgroundColor,
+            height: "30px",
+            width: "30px",
+          }}
+        />
+      );
+    }
+    return shapes;
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "5px",
+        flexWrap: "wrap",
+      }}
+    >
+      {generateShapes("rectangle", "brown").map((shape, index) => shape)}
+    </div>
+  );
 }
